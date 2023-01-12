7411e253def7b70512b54b40adddecc62b972fa8
benpankow <ben@elementl.com>
2023-01-12 08:41:21 -0800

    defs

--- my_project/__init__.py
+++ my_project/__init__.py
@@ -1,1 +1,15 @@
-from .repository import my_repository
+import os
+from dagster import (
+    load_assets_from_package_module,
+    AssetSelection,
+    define_asset_job,
+    ScheduleDefinition,
+    Definitions,
+)
+from dagster_dbt import load_assets_from_dbt_project, dbt_cli_resource
+
+from dagster._utils import file_relative_path
+
+from my_project.assets import population, forecasting
+
+defs = Definitions()
