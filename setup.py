a71c0b7e6bbf94b2929e2fba635633fe7b2a8a82
yuhan <yuhan@elementl.com>
2023-02-23 00:36:20 -0800

    remove workspace, requirements.txt -> setup.py, add pyproject

--- setup.py
+++ setup.py
@@ -3,5 +3,18 @@ import setuptools
 setuptools.setup(
     name="my_project",
     packages=setuptools.find_packages(exclude=["my_project_tests"]),
-    install_requires=["dagster==0+dev", "dagit==0+dev", "pytest", "dbt-duckdb"],
+    install_requires=[
+        "dagster",
+        "dagster-cloud",
+        "dagster-dbt",
+        "dagster_snowflake",
+        "dagster_snowflake_pandas",
+        "dagster_dbt",
+        "lxml",
+        "dbt-duckdb",
+        "dbt-snowflake",
+        "pandas",
+        "dagit",
+        "pytest",
+    ],
 )
