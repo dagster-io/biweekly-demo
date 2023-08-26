020285194a33d3a4362b2e08dd65b15c16654697
Tim Castillo <tim@dagsterlabs.com>
2023-08-26 15:51:09 -0400

    forgot a groupby

--- my_project_start/assets/__init__.py
+++ my_project_start/assets/__init__.py
@@ -5,18 +5,19 @@ import pandas as pd
 import duckdb
 
 @asset
-def country_population() -> None:
-    df = pd.read_html("https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)")[0]
+def country_population():
+    df = pd.read_html(
+        "https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)"
+    )[0]
 
     df.columns = ["country", "region", "subregion", "pop_2018", "pop_2019", "pct_change"]
 
-    df.to_csv("data/population.csv", index=False)
-
+    df.to_csv("data/country_population.csv", index=False)
 
 @asset(
     deps=[country_population]
 )
-def continent_population() -> None:
+def continent_population():
     conn = duckdb.connect()
 
     query = """
@@ -24,7 +25,7 @@ def continent_population() -> None:
             region as continent,
             count(1) as population_count
         from 'data/country_population.csv'
-        group by 1
+        group by region
     """
 
     df = conn.execute(query).fetch_df()
